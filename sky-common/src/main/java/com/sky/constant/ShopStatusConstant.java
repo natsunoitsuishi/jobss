0e3c6c7c059cb1898a85cc823a6d39dd934ecdab
natsuno <linsukida@gmail.com>
2025-08-16 18:57:20 +0900

    UpdateAndShopStatus

---
+++ sky-common/src/main/java/com/sky/constant/ShopStatusConstant.java
@@ -0,0 +1,6 @@
+package com.sky.constant;
+
+public class ShopStatusConstant {
+    public static final String SHOP_OPEN = "1";
+    public static final String SHOP_CLOSE = "0";
+}
---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,70 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import com.sky.vo.DishVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/dish")
+@Api(tags = "dish Port")
+@Slf4j
+
+public class DishController {
+
+    @Autowired
+    private DishService dishService;
+
+    @PostMapping
+    @ApiOperation(value = "new Dish")
+    public Result save (@RequestBody DishDTO dishDTO,
+                        @RequestHeader(value = "Content-Type", required = false) String header) {
+        log.info("RequestHeader :{}", header);
+        log.info("DishDTO : {}", dishDTO);
+
+        dishService.saveWithFlavor(dishDTO);
+        return Result.success();
+    }
+
+    @GetMapping("/page")
+    @ApiOperation(value = "new page")
+    public Result<PageResult> page (DishPageQueryDTO dishPageQueryDTO) {
+        log.info("new page :{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    @DeleteMapping
+    @ApiOperation(value = "deleteByIds")
+    public Result deleteByIds(@RequestParam List<Long> ids) {
+        log.info("deleteByIds :{}", ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "getById")
+    public Result<DishVO> getById(@PathVariable Long id) {
+        log.info("getById : {}", id);
+        DishVO dishVO = dishService.getByIdWithFlavor (id);
+        return Result.success(dishVO);
+    }
+
+    @PutMapping
+    @ApiOperation(value = "update")
+    public Result update (@RequestBody DishDTO dishDTO) {
+        log.info("update :{}", dishDTO);
+        dishService.updateWithFlavor (dishDTO);
+        return Result.success();
+    }
+}
+
